0ed1875e2aec6677630c1e164645fce2be07078c
mark-ruuan <40922189+mark-ruuan@users.noreply.github.com>
2018-07-31 14:08:41 -0700

    Create Circles.cpp

---
+++ JUST Collegiate Programming Contest -2018-/Circles.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+#define fast ios_base::sync_with_stdio(0);cin.tie(NULL);cout.tie(NULL)
+#define ll long long int
+using namespace std;
+const int N = 1e6 + 5;
+const ll inf = -1e18;
+const int MOD = 1e9 + 7;
+const double pi = 3.141592653589793238;
+
+
+
+int main(){
+   fast;
+   ll t;
+   cin >> t;
+   while(t--){
+	   ll a, b;
+	   long double d;
+	   cin >> a >> b >> d;
+	   long double area1 = d * d / 2.0;
+	   cout << fixed << setprecision(12) << area1 << "\n";
+   }
+
+
+
+
+
+
+   return 0;
+} 
